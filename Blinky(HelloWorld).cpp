4dae479c061498bc8433a4459ddc506e969b5076
Charlotte Pieters <charlotte@pietersdekeyser.net>
2019-02-16 20:13:31 +0100

    test i2C fail

--- Blinky(HelloWorld).cpp
+++ Blinky(HelloWorld).cpp
@@ -2,10 +2,23 @@
 
 DigitalOut myled(LED1);
 Serial pc(SERIAL_TX, SERIAL_RX); // tx, rx
+I2C i2c(I2C_SDA, I2C_SCL);
+
 int main() {
-    pc.printf("test dubbel");
+    
+    pc.printf(" test dubbel charlotte");
     while(1) {
-        pc.printf("Hello World !\r\n");
+        char buffer[1] = {0x00};
+        pc.printf(" Hello World !\r \n");
+        pc.printf(" laten we dit nog een beetje verder testen.\r \n ");
+        if(i2c.write(0x1B, buffer, 1, false) != 1){
+            pc.printf("failed to set pointer \r \n");
+        }
+        else{printf("YESSSS");}
+        if(i2c.read(0x1B,buffer,1, false) != 1){
+            pc.printf("failed to read from device");
+        }
+        pc.printf(" get chip id: %d", buffer[0]);
         myled = 1;
         wait(0.5);
         myled = 0;
