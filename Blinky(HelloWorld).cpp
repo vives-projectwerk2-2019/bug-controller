fcb1ece87efaf44225f2f1945896045eca079c5c
Charlotte Pieters <charlotte@pietersdekeyser.net>
2019-02-16 18:25:07 +0100

    added serial connection to monitor the output

--- Blinky(HelloWorld).cpp
+++ Blinky(HelloWorld).cpp
@@ -1,9 +1,11 @@
 #include "mbed.h"
 
 DigitalOut myled(LED1);
-
+Serial pc(SERIAL_TX, SERIAL_RX); // tx, rx
 int main() {
+    pc.printf("test dubbel");
     while(1) {
+        pc.printf("Hello World !\r\n");
         myled = 1;
         wait(0.5);
         myled = 0;
