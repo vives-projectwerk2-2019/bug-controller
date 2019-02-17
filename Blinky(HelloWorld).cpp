34316632907675c7ca387f34640fb0f565e18fff
Charlotte Pieters <charlotte@pietersdekeyser.net>
2019-02-17 13:47:57 +0100

    added classes for QT1070

--- Blinky(HelloWorld).cpp
+++ Blinky(HelloWorld).cpp
@@ -1,55 +1,51 @@
-/*#include "mbed.h"
-
-DigitalOut myled(LED1);
-Serial pc(SERIAL_TX, SERIAL_RX); // tx, rx
-I2C i2c(I2C_SDA, I2C_SCL);
-
-int main() {
-    
-    pc.printf(" test dubbel charlotte");
-    while(1) {
-        char buffer[1] = {0x00};
-        pc.printf(" Hello World !\r \n");
-        pc.printf(" laten we dit nog een beetje verder testen.\r \n ");
-        if(i2c.write(0x1B<<1, buffer, 1) != 1){
-            pc.printf("failed to set pointer \r \n");
-        }
-        else{printf("YESSSS");}
-        if(i2c.read(0x1B<<1,buffer,1) != 1){
-            pc.printf("failed to read from device");
-        }
-        pc.printf(" get chip id: %d", buffer[0]);
-        myled = 1;
-        wait(0.5);
-        myled = 0;
-        wait(0.5);
-    }
-}*/
 #include "mbed.h"
- 
-#define RELAY_BOARD_ADDR (0x1B<<1)
-#define LOOP_WAIT_TIME   2.0
-//I2C i2c(PB_9, PB_8);
+#include "QT1070.h"
+#include "TLC59116.h"
+#define LOOP_WAIT_TIME   0.5
+
 I2C i2c(I2C_SDA, I2C_SCL);
 Serial pc(SERIAL_TX, SERIAL_RX); // tx, rx
 DigitalOut myled(LED1);
  
 int main()
 {
- 
-    char buffer[1];
-    char buffer2[5];
-    buffer[0] = 0x03;
-     
+    QT1070 qt1070(&i2c);
+    pc.printf("Chip id = %d \r \n", qt1070.get_chip_id());
+    wait(0.5);
+    int val;
     while (1) {
-        pc.printf("wat is het res: %d \r \n",i2c.write(0x1B<<1, buffer, 1, 0));
-        wait(1);
-        pc.printf("wat is het res2: %d \r \n",i2c.read(0x1B<<1, buffer2, 1, 0));
+        val = qt1070.get_value();
+        switch (val)
+        {
+        case 1:
+            cout << "rechts" << endl;
+            break;
+        case 2:
+            
+            cout << "links" << endl;
+            break;
+        case 4:
+            cout << "vooruit" << endl;
+            break;
+        case 8:
+            cout << "achteruit" << endl;
+            break;
+        case 16:
+            cout << "actie B" << endl;
+            break;
+        case 32:
+            cout << "actie A" << endl;
+            break;
+        case 64:
+            cout << "actie X" << endl;
+            break;
+        default:
+            cout << "geen bediening" << endl;
+            break;
+        }
         myled = !myled;
-        wait(1);
-        pc.printf("Button pressed %d \r \n ", (int) buffer2[0]);
- 
         wait(LOOP_WAIT_TIME);
+        pc.printf("Pressed button value = %d \r \n", val);
     }
  
 }
