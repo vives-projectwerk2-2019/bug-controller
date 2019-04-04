d74f799094a4f5569034fbdc90d8bc588e398e80
Kevin Janssens <kevin.janssens@student.vives.be>
2019-04-04 18:20:46 +0200

    veranderen locatie files

---
+++ src/Array8bits.cpp
@@ -0,0 +1,54 @@
+#include "mbed.h"
+#include "Array8bits.h"
+
+Array8Bits::Array8Bits()
+{
+  size = 0;
+  buffer = (uint8_t*) malloc(size);
+}
+
+Array8Bits::~Array8Bits()
+{
+  free(buffer);
+}
+
+Array8Bits& Array8Bits::addUint8(uint16_t value)
+{
+  reallocBuffer(1).writeUint16(value);
+  return *this;
+}
+
+Array8Bits& Array8Bits::addArray(uint8_t* value, uint8_t size)
+{
+  for(int i = 0; i < size; i++){
+    addUint8(value[i]);
+  }
+  return *this;
+}
+
+
+
+LoRaEncoder Array8Bits::reallocBuffer(int delta) 
+{
+    void* temp = realloc(buffer, (size + delta) * sizeof(uint8_t));
+    if (temp == NULL) {
+        free(buffer);
+        printf("bad memory allocation!");
+        while(true);
+    } else {
+        buffer = (uint8_t*) temp;
+    }
+    LoRaEncoder encoder(buffer + size);
+    size += delta;
+    return encoder;
+}
+
+uint8_t* Array8Bits::getMessage()
+{
+  return buffer;
+}
+
+uint8_t Array8Bits::getLength()
+{
+  return size;
+}
---
+++ src/Array8bits.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "stdint.h"
+#include "LoRaEncoder.h"
+#include "LoRaMessage.h"
+
+class Array8Bits 
+{
+  public:
+    Array8Bits();
+    ~Array8Bits();
+    LoRaMessage addon;
+
+    Array8Bits& addUint8(uint16_t value);    
+    Array8Bits& addArray(uint8_t* value, uint8_t size);
+
+    LoRaEncoder reallocBuffer(int delta);
+    uint8_t* getMessage();
+    uint8_t getLength();
+
+  private:
+    uint8_t* buffer;
+    uint8_t size;
+};
